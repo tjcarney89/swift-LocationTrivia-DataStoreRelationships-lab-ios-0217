15b04f5ee8b6b924cf9a4d76cc63ed6d5bf93f33
ianrahman <ian.alexander.rahman@gmail.com>
2016-07-04 14:14:02 -0400

    Wrote AddLocationViewController

--- swift-LocationTrivia-DataStoreRelationships-lab/AddLocationViewController.swift
+++ swift-LocationTrivia-DataStoreRelationships-lab/AddLocationViewController.swift
@@ -7,29 +7,94 @@
 //
 
 import UIKit
+import CoreLocation
 
-class AddLocationViewController: UIViewController {
+class AddLocationViewController: UIViewController, CLLocationManagerDelegate {
 
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var latitudeField: UITextField!
+    @IBOutlet weak var longitudeField: UITextField!
+    
+    @IBOutlet weak var useCurrentLocationButton: UIButton!
+    @IBOutlet weak var submitButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    var locationManager = CLLocationManager()
+    var currentLocation = CLLocation()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        self.setupAccessibilityLabels()
+        self.setupLocationManager()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewDidDisappear(animated: Bool) {
+        super.viewDidDisappear(true)
+        self.locationManager.stopUpdatingLocation()
     }
-    */
+    
+    func setupAccessibilityLabels() {
+        self.nameField.accessibilityLabel = "nameField"
+        self.nameField.accessibilityIdentifier = "nameField"
+        
+        self.latitudeField.accessibilityLabel = "latitudeField"
+        self.latitudeField.accessibilityIdentifier = "latitudeField"
+        
+        self.longitudeField.accessibilityLabel = "longitudeField"
+        self.longitudeField.accessibilityIdentifier = "longitudeField"
+        
+        self.submitButton.accessibilityLabel = "saveButton"
+        self.submitButton.accessibilityIdentifier = "saveButton"
+        
+        self.cancelButton.accessibilityLabel = "cancelButton"
+        self.cancelButton.accessibilityIdentifier = "cancelButton"
+    }
+    
+    
+    // MARK: Location
+    
+    func setupLocationManager() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.distanceFilter = 50
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
+    }
 
+    func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let lastLocation = locations.last {
+            currentLocation = lastLocation
+        }
+    }
+    
+    
+    // MARK: IBActions
+    
+    @IBAction func useCurrentLocation(sender: AnyObject) {
+        self.latitudeField.text = String(format: "%f", self.currentLocation.coordinate.latitude)
+        self.longitudeField.text = String(format: "%f", self.currentLocation.coordinate.longitude)
+    }
+    
+    @IBAction func submitButtonTapped(sender: AnyObject) {
+        var latitude: Float
+        var longitude: Float
+        
+        if let latitudeText = self.latitudeField.text {
+            latitude = Float(latitudeText)!
+            if let longitudeText = self.longitudeField.text {
+                longitude = Float(longitudeText)!
+                if let name = self.nameField.text {
+                    let newLocation = Location.init(name: name, latitude: latitude, longitude: longitude)
+                    LocationsDataStore.sharedInstance.locations.append(newLocation)
+                }
+                
+                self.dismissViewControllerAnimated(true, completion: nil)
+            }
+        }
+    }
+    
+    @IBAction func cancelButtonTapped(sender: AnyObject) {
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 }
